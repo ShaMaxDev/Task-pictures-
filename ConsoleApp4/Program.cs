877e96eaeca4a28f3b62c2e82a85107ca069d667
ShaMaxDev <maksimbychkov@yahoo.com>
2023-09-11 20:35:21 -0700

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int picturesInLine = 3;
+
+            int lines = pictures / picturesInLine;
+            int extaPictures = pictures % picturesInLine;
+            Console.WriteLine(lines + " Lines and " + extaPictures + " extra pictures "); 
+
+        }
+    }
+}
